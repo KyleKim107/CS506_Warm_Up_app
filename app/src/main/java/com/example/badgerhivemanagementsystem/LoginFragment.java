d89c00701cec6452b7682d7674a62232290c21a7
adrii4 <adri.regalado@live.com>
2020-09-30 23:16:10 -0400

    app now successfully logs in users

--- app/src/main/java/com/example/badgerhivemanagementsystem/LoginFragment.java
+++ app/src/main/java/com/example/badgerhivemanagementsystem/LoginFragment.java
@@ -29,20 +29,10 @@ public class LoginFragment extends Fragment {
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * This method sets up the Register Fragment
      */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
+    public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
--- app/src/main/java/com/example/badgerhivemanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/badgerhivemanagementsystem/MainActivity.java
@@ -3,25 +3,44 @@ package com.example.badgerhivemanagementsystem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
-    private Button btn_edit_pic;
-    public FirebaseAuth mAuth;
-    String username;
-    String email;
-    String apiary_address;
-    String phone_number;
+    Button mEditPicture;
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mSignUpBtn;
+    Button mSignInBtn;
+    TextView mSignInTxt;
+    TextView mSignUpTxt;
+    FirebaseAuth mAuth;
+    String fullName, email, password, phone;
+    String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.setSelectedItemId(R.id.nav_profile);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,
                 new ProfileFragment()).commit();
 
         // Initialize Firebase Auth
@@ -40,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         hideBottomBar(true);
 
-        //btn_photo_upload = findViewById(R.id.btn_photo);
+        onStart();
     }
 
     @Override
@@ -48,10 +67,150 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        // updateUI(currentUser);
+        if (currentUser == null) {
+            openFragment(LoginFragment.newInstance());
+        } else {
+            //TODO
+            createUI(currentUser);
+        }
     }
 
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
+    public void signUp(View view) {
+        openFragment(RegisterFragment.newInstance());
+    }
+
+    public void signIn(View view) {
+        openFragment(LoginFragment.newInstance());
+    }
+
+    public void login(View view) {
+        EditText email_text = findViewById(R.id.email);
+        EditText password_text = findViewById(R.id.password);
+
+        email = email_text.getText().toString();
+        password = password_text.getText().toString();
+
+        signInWithEmailAndPassword(email, password);
+    }
+
+    public void signInWithEmailAndPassword(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            id = mAuth.getCurrentUser().getUid();
+                            createUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void register(View view) {
+        mEditPicture = findViewById(R.id.editpic_register);
+        mFullName = findViewById(R.id.fullname_register);
+        mEmail = findViewById(R.id.email_register);
+        mPassword = findViewById(R.id.password_register);
+        mPhone = findViewById(R.id.phone_register);
+        mSignUpBtn = findViewById(R.id.signup_register);
+        mSignInTxt = findViewById(R.id.signin_register);
+
+        fullName = mFullName.getText().toString().trim();
+        email = mEmail.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        phone = mPhone.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            mFullName.setError("Full name required.");
+            mFullName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            mEmail.setError("Email address required.");
+            mEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("Enter a valid email address.");
+            mEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            mPassword.setError("Password required.");
+            mPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            mPassword.setError("Password should be at least 6 characters long.");
+            mPassword.requestFocus();
+            return;
+        }
+
+        if (phone.isEmpty()) {
+            mPhone.setError("Phone required.");
+            mPhone.requestFocus();
+            return;
+        }
+
+        if (!Patterns.PHONE.matcher(phone).matches()) {
+            mPhone.setError("Enter a valid phone number.");
+            mPhone.requestFocus();
+            return;
+        }
+
+        createAccount(email, password);
+    }
+
+    public void createAccount(String email_address, String password) {
+        mAuth.createUserWithEmailAndPassword(email_address, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser curr_user = mAuth.getCurrentUser();
+                            createUI(curr_user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void createUI(FirebaseUser user) {
+        // User has just signed up - update display name
+        if (user != null) {
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(fullName)
+                    .build();
+            user.updateProfile(profile);
+        }
+
+        openFragment(ProfileFragment.newInstance("", ""));
+        hideBottomBar(false);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -68,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
                             break;
                     }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,
                             selectedFragment).commit();
 
                     return true;
